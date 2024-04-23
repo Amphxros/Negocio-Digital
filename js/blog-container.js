84d8384cebedc471b28a54b433128f0a9077e286
A. Rubio <ampxrubio@gmail.com>
2024-04-23 18:28:02 +0100

    blog en json del todo

--- js/blog-container.js
+++ js/blog-container.js
@@ -1,41 +1,39 @@
 class BlogList {
-    constructor() {
-
+    constructor(cart) {
+      this.cart = cart;
       this.container = document.querySelector('.blog-container');
       this.productService = new BlogService();
       this.sortDirection = 'ascending';
       this.productService
         .getBlog()
-        .then(() => this.renderProducts())
+        .then(() => this.renderBlog())
         .then(() => this.addEventListeners()); 
       document.querySelector('.search').addEventListener('keydown', async () => {
-        await this.renderProducts();
+        await this.renderBlog();
         this.addEventListeners();
       });   
     }
-    async renderProducts() {
+    async renderBlog() {
       const searchInput = document.querySelector('.search');
       let productListDomString = '';
       const products = await this.productService.getBlog();
       [...products]
         .filter( product => product.title.includes(searchInput.value) )
         .sort( (a, b) => this.sortDirection === 'ascending' 
-                           ? a.price - b.price
-                           : b.price - a.price)
+                           ? a.id - b.id
+                           : b.id - a.id)
         .forEach(product => {
         productListDomString += `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                     <div class="card product">
-                      <img class="card-img-top" src="${product.image}" 
-                          alt="${product.title}">
+                     
                       <div class="card-body d-flex flex-column">
                         <h4 class="card-title">${product.title}</h4>
                         <p class="card-text flex-fill">${product.description}</p>
+                        <img class="card-img-top" src="${product.image}" 
+                        alt="${product.title}">
                         <div class="d-flex justify-content-around">
                           <button class="btn btn-info" data-bs-toggle="modal"
                             data-bs-target="#productInfoModal" data-id="${product.id}">Info
-                          </button>
-                          <button class="btn btn-primary buy" data-id="${product.id}">
-                            $${product.price} - Buy
                           </button>
                         </div>
                       </div>
@@ -75,20 +73,12 @@ class BlogList {
     async handleProductInfoClick(event) {
       const button = event.target; // Button that triggered the modal
       const id = button.dataset.id; // Extract info from data-* attributes
-      const product = await this.productService.getProductById(id);
+      const product = await this.productService.getBlogById(id);
       const modal = document.querySelector('#productInfoModal');
-      const productImg1 = modal.querySelector('.modal-body .image-one');
-      productImg1.setAttribute('src', product.additionalImages[0]);
-      productImg1.setAttribute('alt', product.additionalImages[0]);
-      const productImg2 = modal.querySelector('.modal-body .image-two');
-      productImg2.setAttribute('src', product.additionalImages[1]);
-      productImg2.setAttribute('alt', product.additionalImages[1]);
+      
       modal.querySelector('.modal-body .card-title').innerText = product.title;
-      modal.querySelector('.modal-body .card-text').innerText =
-        product.description;
-      const btnBuy = modal.querySelector('button.buy');
-      btnBuy.innerText = `${product.price} - Buy`;
-      btnBuy.dataset.id = id;
+    modal.querySelector('.modal-body .card-text').innerText =
+        product.description + "\n \n" + product.content;
     }
     handleProductBuyClick(event) {
       const button = event.target;
